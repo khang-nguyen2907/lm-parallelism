befdfea51f1b3333a8b4ac3d2179709b1374b71d
khang-nguyen2907 <giakhang29079@gmail.com>
2023-04-28 10:00:26 +0700

    init commit

---
+++ scripts/run/run_opt_ds.sh
@@ -0,0 +1,12 @@
+accelerate launch --config_file lm-parallelism/src/configs/accelerate/accelerate_dp.yaml src/opt/opt_clm.py \
+    --model_name_or_path facebook/opt-1.3b \
+    --dataset_name wikitext \
+    --dataset_config_name wikitext-2-raw-v1 \
+    --per_device_train_batch_size 8 \
+    --per_device_eval_batch_size 8 \
+    --do_train \
+    --do_eval \
+    --output_dir models/opt-clm-ds \
+    --block_size 1024 \
+    --with_tracking \
+    --report_to tensorboard
---
+++ scripts/run/run_t5_ds.sh
@@ -0,0 +1,13 @@
+accelerate launch --config_file lm-parallelism/src/configs/accelerate/accelerate_dp.yaml src/t5/t5_summarization.py \
+    --model_name_or_path t5-3b \
+    --dataset_name cnn_dailymail \
+    --dataset_config "3.0.0" \
+    --source_prefix "summarize: " \
+    --output_dir models/t5-summarization-ds \
+    --pad_to_max_length \
+    --max_source_length 1024 \
+    --max_target_length 512 \
+    --per_device_train_batch_size 8 \
+    --per_device_eval_batch_size 4 \
+    --with_tracking \
+    --report_to tensorboard
